cd2d233fcdd4054c59912b03eb694667b7ccd945
Umut Buğra TER <umutbugra.ter.06@outlook.com>
2023-06-29 15:07:22 +0300

    bin packing next fit algorithm

---
+++ BinPacking_NextFit/BinPacking_NextFit/MyNextFit.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace BinPacking_NextFit
+{
+    class MyNextFit
+    {
+        int İlkAlan = 0;        // kutunun ilk kapladığı alanı tutar.
+        int SonAlan = 0;       // kutuda yaptığımız işlemlerin en son ne kadarlık yer bıraktığını tutar.
+        int KutuSayısı = 1;   // kaç tane kutu oluşturduğumuzu saymayı tutar.
+        int i_kalan = 0;     // for döngüsünde her bir kutu içine giren , son paketin indexini tutar.
+        int i_bas=0;        // for döngüsünde her bir kutu içine giren , ilk paketin indexini tutar.
+
+        /// <summary>
+        /// Paketleme işleminin yapıldığı ana fonksiyondur.
+        /// </summary>
+        /// <param name="PaketBoyutu_"> Herbir paketin boyutunu tutar. </param>
+        /// <param name="PaketSayısı_"> Toplam paketin sayısını tutar. </param>
+        /// <param name="KutununKaplayacağıYer_"> Kutunun toplam alanını veya kağladığı yeri tutar. </param>
+        public void İslem(int[] PaketBoyutu_, int PaketSayısı_, int KutununKaplayacağıYer_)
+        {
+            İlkAlan = KutununKaplayacağıYer_;
+            for (int i = 0; i < PaketSayısı_; i++)
+            {
+                if(KutununKaplayacağıYer_ >= PaketBoyutu_[i])
+                {
+                    KutununKaplayacağıYer_ -= PaketBoyutu_[i];
+                    SonAlan = KutununKaplayacağıYer_;
+                    i_kalan = i;
+                }
+                else
+                {
+                    if (PaketBoyutu_[i] > İlkAlan) 
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("GİRDİĞİNİZ "+PaketBoyutu_[i]+" BOYUTLU PAKET, BOYUTU KUTUNUN HACMİNDEN" +
+                            " FAZLA OLDUĞU İÇİN BARINDIRILAMAZ.");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        for (int j = i_bas; j <= i_kalan;j++)
+                        {
+                            if (PaketBoyutu_[j]<İlkAlan)
+                            {
+                                Console.WriteLine(" {0}. kutuya giren elemanlar = {1}", KutuSayısı, PaketBoyutu_[j]);
+                            }
+                        }
+                        i_bas = i_kalan + 1;
+                        Console.WriteLine(" Sıradaki paket {0}. kutudan kalan alandan daha büyüktür. ", KutuSayısı);
+                        Console.WriteLine(" {0}. kutudan kalan alan = {1}",KutuSayısı,SonAlan);
+                        Console.WriteLine("************************************");Console.WriteLine(" {0}. bir yeni kutu oluşturuldu.", KutuSayısı + 1); 
+                        KutununKaplayacağıYer_ = İlkAlan;
+                        KutuSayısı++;
+                        --i;
+                    }
+                }
+            }
+            for (int j = i_bas; j <= i_kalan; j++)
+            {
+                if (PaketBoyutu_[j] < İlkAlan)
+                {
+                    Console.WriteLine(" {0}. kutuya giren elemanlar = {1}", KutuSayısı, PaketBoyutu_[j]);
+                }
+            }
+            Console.WriteLine(" {0}. kutudan kalan alan = {1}",KutuSayısı,SonAlan);
+            Console.WriteLine("------------GENEL ÖZET---------------");
+            Console.WriteLine(KutuSayısı+ " ADET KUTU OLUŞTURULDU.");
+                
+        }
+        /// <summary>
+        /// Tek başına kutunun alanından büyük olan paketlerin sayısının tutulduğu fonksiyondur
+        /// </summary>
+        /// <param name="PaketBoyutu_"> Herbir paketin boyutunu tutar. </param>
+        /// <param name="PaketSayısı_"> Toplam paketin sayısını tutar. </param>
+        /// <param name="KutununKaplayacağıYer_"> Kutunun toplam alanını veya kapladığı yeri tutar. </param>
+        public void Bos(int[] PaketBoyutu_, int PaketSayısı_, int KutununKaplayacağıYer_)
+        { 
+            int DışarıdaKalanPaket = 0;
+            for (int i = 0; i < PaketSayısı_; i++)
+            {
+                if (PaketBoyutu_[i] > KutununKaplayacağıYer_)
+                {
+                    DışarıdaKalanPaket++;
+                }
+            }
+            Console.WriteLine(DışarıdaKalanPaket + " ADET KUTUYA GİRMEYEN PAKET VARDIR.");
+        }
+    }
+}
---
+++ BinPacking_NextFit/BinPacking_NextFit/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+/* 
+    First, let's consider the existence of an object called a box. Since the inside of this box occupies a certain area, first that area
+    we determine. Then we place objects (packages) in order to fill this box. Back of the box
+    If the remaining area is smaller than the size of the package, we create a new box and throw it into the new box. If the size of a package differs from the size of the box
+    When it is more, it stays outside because it cannot enter logically, so we cannot put it inside.
+ */
+
+/*
+    İlk olarak kutu adında bir cismin varlığını düşünelim. Bu kutunun içi belli bir alan yer kapladığı için, ilk o alanı
+    belirliyoruz. Daha sonra bu kutunun içini doldurması için SIRASIYLA cisimler(paketler) yerleştiriyoruz. Kutunun geriye
+    kalan alanı, paketin boyutundan küçükse yeni bir kutu oluşturup yeni kutunun içine atıyoruz. Eğer bir paketin boyutu, kutunun boyutundan
+    daha fazla olduğunda mantıken giremeyeceği için dışarıda kalıyor yani içine koyamıyoruz.
+*/
+
+namespace BinPacking_NextFit
+{
+    class Program
+    {
+        /// <summary>
+        /// Kutunun hacmini , paket sayısının ve paketin boyutunu yani büyüklüğünü kullanıcı tarafından girilerek
+        /// paketleme işlemimize başlanacaktır. Try-Catch bloklarıyla ise kullanıcı yanlış değerler girer ise 
+        /// kullanıcı uyaracak ve alınabilir değeri girmesi istenecektir.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            
+            int PaketSayısı=0, KutununKaplayacağıYer=0;
+            try
+            {
+                Console.WriteLine("KUTUNUZUN BOYUTUNU GİRİNİZ ?");
+                KutununKaplayacağıYer = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine( "Kutunuza , toplam " +KutununKaplayacağıYer+ " boyutlu paket alabilecek kapasite belirlediniz.. ");
+            }
+            catch 
+            {
+                Console.WriteLine(" HATAA... Programı yeniden başlatıp sayısal değer giriniz.");
+            }
+            try
+            {
+                Console.WriteLine("KUTUNUN İÇİNE KAÇ TANE PAKET YERLEŞTİRMEK İSTİYORSUNUZ ?");
+                PaketSayısı = Convert.ToInt32(Console.ReadLine());
+            }
+            catch 
+            {
+                Console.WriteLine(" HATAA... Programı yeniden başlatıp sayısal değer giriniz.");
+            }
+            int[] paketboyutu = new int[PaketSayısı];
+            Console.WriteLine("PAKETLERİNİZİN BOYUTLARINI GİRİNİZ ");
+            for (int i = 0; i < PaketSayısı; i++)
+            {
+                Console.Write((i+1)+ ". paket boyutu = ");
+                paketboyutu[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            MyNextFit myNextFit = new MyNextFit();
+            myNextFit.İslem(paketboyutu, PaketSayısı, KutununKaplayacağıYer);
+            myNextFit.Bos(paketboyutu, PaketSayısı, KutununKaplayacağıYer);
+        }
+    }
+}
